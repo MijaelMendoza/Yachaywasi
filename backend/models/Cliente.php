548fbbf360be44fd37db6bfa3b92056d89afe5c9
MijaelMendoza <mijaelmendoza.h@gmail.com>
2024-04-22 22:37:48 -0400

    Modelos de cliente y empleado

---
+++ backend/models/Cliente.php
@@ -0,0 +1,53 @@
+<?php
+class Cliente {
+    private $db;
+
+    public function __construct() {
+        $this->db = require_once '.././core/conexion.php';
+    }
+
+    public function obtenerClientes() {
+        $stmt = $this->db->prepare("SELECT * FROM Cliente");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function obtenerClientePorId($cu) {
+        $stmt = $this->db->prepare("SELECT * FROM Cliente WHERE cu = :cu");
+        $stmt->bindParam(':cu', $cu, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function agregarCliente($nombre, $ci, $direccion, $telefono, $correo, $fecha_registro, $segmento_cliente) {
+        $stmt = $this->db->prepare("INSERT INTO Cliente (nombre, ci, direccion, telefono, correo, fecha_registro, segmento_cliente) VALUES (:nombre, :ci, :direccion, :telefono, :correo, :fecha_registro, :segmento_cliente)");
+        $stmt->bindParam(':nombre', $nombre);
+        $stmt->bindParam(':ci', $ci);
+        $stmt->bindParam(':direccion', $direccion);
+        $stmt->bindParam(':telefono', $telefono);
+        $stmt->bindParam(':correo', $correo);
+        $stmt->bindParam(':fecha_registro', $fecha_registro);
+        $stmt->bindParam(':segmento_cliente', $segmento_cliente);
+        $stmt->execute();
+    }
+
+    public function actualizarCliente($cu, $nombre, $ci, $direccion, $telefono, $correo, $fecha_registro, $segmento_cliente) {
+        $stmt = $this->db->prepare("UPDATE Cliente SET nombre = :nombre, ci = :ci, direccion = :direccion, telefono = :telefono, correo = :correo, fecha_registro = :fecha_registro, segmento_cliente = :segmento_cliente WHERE cu = :cu");
+        $stmt->bindParam(':nombre', $nombre);
+        $stmt->bindParam(':ci', $ci);
+        $stmt->bindParam(':direccion', $direccion);
+        $stmt->bindParam(':telefono', $telefono);
+        $stmt->bindParam(':correo', $correo);
+        $stmt->bindParam(':fecha_registro', $fecha_registro);
+        $stmt->bindParam(':segmento_cliente', $segmento_cliente);
+        $stmt->bindParam(':cu', $cu);
+        $stmt->execute();
+    }
+
+    public function eliminarCliente($cu) {
+        $stmt = $this->db->prepare("UPDATE Cliente WHERE cu = :cu SET estado = false");
+        $stmt->bindParam(':cu', $cu, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+}
+?>
---
+++ backend/models/Empleado.php
@@ -0,0 +1,61 @@
+<?php
+class Empleado {
+    private $db;
+
+    public function __construct() {
+        $this->db = require_once '.././core/conexion.php';
+    }
+    
+    public function obtenerEmpleados() {
+        $stmt = $this->db->prepare("SELECT * FROM Empleado");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function obtenerEmpleadoPorId($ca) {
+        $stmt = $this->db->prepare("SELECT * FROM Empleado WHERE ca = :ca");
+        $stmt->bindParam(':ca', $ca, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function agregarEmpleado($nombre, $ci, $password, $direccion, $telefono, $correo, $cargo, $fecha_contratacion, $salario, $estado, $Sucursal_cs) {
+        $stmt = $this->db->prepare("INSERT INTO Empleado (nombre, ci, password, direccion, telefono, correo, cargo, fecha_contratacion, salario, estado, Sucursal_cs) VALUES (:nombre, :ci, :password, :direccion, :telefono, :correo, :cargo, :fecha_contratacion, :salario, :estado, :Sucursal_cs)");
+        $stmt->bindParam(':nombre', $nombre);
+        $stmt->bindParam(':ci', $ci);
+        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':direccion', $direccion);
+        $stmt->bindParam(':telefono', $telefono);
+        $stmt->bindParam(':correo', $correo);
+        $stmt->bindParam(':cargo', $cargo);
+        $stmt->bindParam(':fecha_contratacion', $fecha_contratacion);
+        $stmt->bindParam(':salario', $salario);
+        $stmt->bindParam(':estado', $estado, PDO::PARAM_BOOL);
+        $stmt->bindParam(':Sucursal_cs', $Sucursal_cs, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+
+    public function actualizarEmpleado($ca, $nombre, $ci, $password, $direccion, $telefono, $correo, $cargo, $fecha_contratacion, $salario, $estado, $Sucursal_cs) {
+        $stmt = $this->db->prepare("UPDATE Empleado SET nombre = :nombre, ci = :ci, password = :password, direccion = :direccion, telefono = :telefono, correo = :correo, cargo = :cargo, fecha_contratacion = :fecha_contratacion, salario = :salario, estado = :estado, Sucursal_cs = :Sucursal_cs WHERE ca = :ca");
+        $stmt->bindParam(':nombre', $nombre);
+        $stmt->bindParam(':ci', $ci);
+        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':direccion', $direccion);
+        $stmt->bindParam(':telefono', $telefono);
+        $stmt->bindParam(':correo', $correo);
+        $stmt->bindParam(':cargo', $cargo);
+        $stmt->bindParam(':fecha_contratacion', $fecha_contratacion);
+        $stmt->bindParam(':salario', $salario);
+        $stmt->bindParam(':estado', $estado, PDO::PARAM_BOOL);
+        $stmt->bindParam(':Sucursal_cs', $Sucursal_cs, PDO::PARAM_INT);
+        $stmt->bindParam(':ca', $ca);
+        $stmt->execute();
+    }
+
+    public function eliminarEmpleado($ca) {
+        $stmt = $this->db->prepare("UPDATE Empleado WHERE ca = :ca SET estado = false");
+        $stmt->bindParam(':ca', $ca, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+}
+?>
