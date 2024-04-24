f44a75dbd6d8d4c7271fe138e90893bc34ca2742
MrBl4ck <franz.carvajal04@gmail.com>
2024-04-24 00:57:59 -0400

    Modelos de libros

---
+++ backend/models/Libros.php
@@ -0,0 +1,44 @@
+<?php
+class Libro
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = require_once '../core/conexion.php';
+    }
+
+    public function obtenerLibros()
+    {
+        $stmt = $this->db->prepare("SELECT * FROM Libros");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function obtenerLibroPorId($cl)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM Libros WHERE cl = :cl");
+        $stmt->bindParam(':cl', $cl, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function agregarLibro($nombre, $genero, $precio, $titulo, $editorial, $anioPublicacion, $stock, $Editorial_ce, $Sucursal_cs)
+    {
+        $stmt = $this->db->prepare("INSERT INTO Libros (nombre, genero, precio, titulo, editorial, anioPublicacion, stock, Editorial_ce, Sucursal_cs) VALUES (:nombre, :genero, :precio, :titulo, :editorial, :anioPublicacion, :stock, :Editorial_ce, :Sucursal_cs)");
+        $stmt->execute();
+    }
+
+    public function actualizarLibro($cl, $nombre, $genero, $precio, $titulo, $editorial, $anioPublicacion, $stock, $Editorial_ce, $Sucursal_cs)
+    {
+        $stmt = $this->db->prepare("UPDATE Libros SET nombre = :nombre, genero = :genero, precio = :precio, titulo = :titulo, editorial = :editorial, anioPublicacion = :anioPublicacion, stock = :stock, Editorial_ce = :Editorial_ce, Sucursal_cs = :Sucursal_cs WHERE cl = :cl");
+        $stmt->execute();
+    }
+
+    public function eliminarLibro($cl)
+    {
+        $stmt = $this->db->prepare("DELETE FROM Libros WHERE cl = :cl");
+        $stmt->bindParam(':cl', $cl, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+}
