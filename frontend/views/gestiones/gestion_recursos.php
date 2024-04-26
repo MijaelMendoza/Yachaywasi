200f90d9ff92de2bb90605a6926f935d1c442a86
yomiisea <yomarrgm@gmail.com>
2024-04-26 01:08:19 -0400

    modalProveedor
    Ya esta el modal para el registro de proveedores

--- frontend/views/gestiones/gestion_recursos.php
+++ frontend/views/gestiones/gestion_recursos.php
@@ -82,7 +82,7 @@ include '../templates/header.php';
                     <input type="hidden" name="user_id"
                         value="<?php echo htmlspecialchars($_SESSION['user_id'] ?? ''); ?>">
                     <button type="button" class="btn btn-outline-primary btn-lg" data-bs-toggle="modal"
-                        data-bs-target="#registroEmpleadoModal">
+                        data-bs-target="#registroProveedorModal">
                         <img src="../../images/registro_venta.png" alt="Proveedor" style="width: 100px;">
                         <div>REGISTRAR PROVEEDOR</div>
                     </button>
@@ -148,7 +148,7 @@ include '../templates/header.php';
                                 <div class="col">
                                     <label for="telefono" class="form-label">Teléfono</label>
                                     <input type="text" class="form-control" id="telefono"
-                                        placeholder="Teléfono">
+                                        placeholder="#######">
                                 </div>
                             </div>
                             <div class="row mb-3">
@@ -210,7 +210,74 @@ include '../templates/header.php';
     </div>
 </div>
 
-
+<!-- Ventana Modal de Registro de Proveedor -->
+<div class="modal fade" id="registroProveedorModal" tabindex="-1" aria-labelledby="registroProveedorModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header bg-danger text-white">
+                <h5 class="modal-title" id="registroProveedorModalLabel">REGISTRAR PROVEEDOR</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <!-- Formulario de registro de proveedor -->
+            <div class="modal-body">
+                <form id="formularioProveedor">
+                    <div class="row mb-3">
+                        <!-- Columna Izquierda -->
+                        <div class="col-md-6">
+                            <div class="row mb-3">
+                                <!-- Campo de Nombre del Proveedor -->
+                                <div class="col">
+                                    <label for="nombreProveedor" class="form-label">Nombre del Proveedor</label>
+                                    <input type="text" class="form-control" id="nombreProveedor"
+                                        placeholder="Nombre del Proveedor">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <!-- Campo de Teléfono -->
+                                <div class="col">
+                                    <label for="telefonoProveedor" class="form-label">Teléfono</label>
+                                    <input type="text" class="form-control" id="telefonoProveedor"
+                                        placeholder="#######">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <!-- Campo de Correo -->
+                                <div class="col">
+                                    <label for="correoProveedor" class="form-label">Correo</label>
+                                    <input type="email" class="form-control" id="correoProveedor"
+                                        placeholder="Correo">
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Columna Derecha -->
+                        <div class="col-md-6">
+                            <div class="row mb-3">
+                                <!-- Campo de Fecha de Registro -->
+                                <div class="col">
+                                    <label for="fechaRegistroProveedor" class="form-label">Fecha de Registro</label>
+                                    <input type="date" class="form-control" id="fechaRegistroProveedor">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <!-- Campo de Contacto -->
+                                <div class="col">
+                                    <label for="contactoProveedor" class="form-label">Contacto </label>
+                                    <input type="text" class="form-control" id="contactoProveedor"
+                                        placeholder="Contacto">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-primary" id="registrarProveedorBtn" disabled>Registrar</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 
     <div class="logout-container">
