b05f7724528ad8664b4c89866709cf386ddafc8d
unknown <ayana.siegle@ucb.edu.bo>
2024-04-27 02:21:23 -0400

    Search funcional
    Se me olvido el id

--- frontend/views/informes/informeInventario.php
+++ frontend/views/informes/informeInventario.php
@@ -61,7 +61,8 @@
     
     <input style="background-image: url('data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 24 24\' fill=\'%23000\' width=\'24px\' height=\'24px\'%3E%3Cpath d=\'M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z\'/%3E%3Cpath d=\'M0 0h24v24H0z\' fill=\'none\'/%3E%3C/svg%3E'); background-size: 20px; background-repeat: no-repeat; background-position: 10px center; padding-left: 40px; margin-left: 83%; width: 15%; border-radius: 40px; margin-top: 3.5%;" type="text" id="searchInput" onkeyup="searchTable()" placeholder="Buscar..">
     <div class="table-responsive">
-        <table class="table table-striped">
+    <table class="table table-striped" id="librosTable">
+
             <thead>
                 <tr>
                     <th>CL</th>
