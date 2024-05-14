a7a58456d6db152b20486e1df7f0769fa82c66a5
MijaelMendoza <mijaelmendoza.h@gmail.com>
2024-05-13 22:41:11 -0400

    Base del dashboard

---
+++ frontend/views/Dashboards/example.php
@@ -0,0 +1,33 @@
+<?php include '../templates/header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            width: 100%;
+            overflow: hidden;
+        }
+
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+            overflow: hidden;
+        }
+    </style>
+</head>
+
+<body>
+    <iframe title="ventasSupermercado(2)"
+        src="https://app.powerbi.com/view?r=eyJrIjoiNGJhYjMyMGEtODE5My00YjgyLWFlZTMtNWI2ZmVlMjEwMzY3IiwidCI6ImNjMjg2MzNmLTEyYjgtNDZjYi1iYzE1LTk1MWRhZTIzOWI0ZCIsImMiOjR9"
+        frameborder="0" allowFullScreen="true"></iframe>
+</body>
+
+</html>
+<?php include '../templates/footer.php'; ?>
--- frontend/views/templates/header.php
+++ frontend/views/templates/header.php
@@ -43,6 +43,7 @@
                         <a class="nav-link" href="../gestiones/gestion_inventario.php">Gestión Inventario</a>
                         <a class="nav-link" href="../gestiones/gestion_recursos.php">Gestión de Recursos</a>
                         <a class="nav-link" href="../informes/informes.php">Informes</a>
+                        <a class="nav-link" href="../Dashboards/example.php">Dashboard</a>
                     </div>
                 </div>
             </div>
